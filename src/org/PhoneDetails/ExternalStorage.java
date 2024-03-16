74d3ff1afa4f6602ad7b2cd478f87651fcd036e1
Mechanical@307 <chsaikumar307@gmial.com>
2024-03-16 16:49:53 +0530

    saikumar pushed

---
+++ src/org/PhoneDetails/ExternalStorage.java
@@ -0,0 +1,10 @@
+package org.PhoneDetails;
+
+public class ExternalStorage {
+    public void externalStorage() {
+    	System.out.println("External Storage is 64gb");
+	}
+	 
+	}
+
+
---
+++ src/org/PhoneDetails/InternalStorage.java
@@ -0,0 +1,18 @@
+package org.PhoneDetails;
+
+public class InternalStorage {
+	
+	public void internalStorages() {
+	  	System.out.println("InternalStorage is 64");
+	  	
+	}
+	public static void main(String[] args) {
+		InternalStorage s  = new InternalStorage();
+		s.internalStorages();
+	
+	
+		ExternalStorage e =new ExternalStorage();
+		e.externalStorage();
+		
+	}
+}
